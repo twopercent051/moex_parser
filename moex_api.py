5baa83de44ccd4b8bd4cd56ed19a110195c2eda8
twopercent051 <zandrej051@gmail.com>
2023-10-07 19:13:32 +0300

    a

--- moex_api.py
+++ moex_api.py
@@ -2,11 +2,11 @@ import requests
 
 import apimoex
 
-# proxy = {"https": "https://91.144.163.169:8090"}
+# proxy = {"https": "http://yhv02e:SP9zBP@46.161.47.116:9358"}
 
 
 with requests.Session() as session:
     # session.proxies.update(proxy)
-    data = apimoex.get_board_history(session, "SNGSP")
+    data = apimoex.get_market_candles(session=session, security="SNGSP", start="2022-10-07", interval=1)
     print(data)
 
